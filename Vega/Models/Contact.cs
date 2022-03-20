7ea975872652c4eb380cd76f00edaabf64e0da69
mhtaldmr <mht.alidemir@gmail.com>
2022-03-20 15:50:44 +0300

    Adding VehicleFeatures.cs for making many to many realtionship between Vehicles and Features

--- Vega/Models/Contact.cs
+++ Vega/Models/Contact.cs
@@ -1,11 +1,17 @@
-﻿namespace Vega.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vega.Models
 {
     public class Contact
     {
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
+        [StringLength(255)]
         public string Email { get; set; }
 
+        [Required]
         public string Phone { get; set; }
     }
 }
--- Vega/Models/Vehicle.cs
+++ Vega/Models/Vehicle.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Vega.Models
@@ -13,12 +15,24 @@ namespace Vega.Models
         [Required]
         [StringLength(255)]
         public string ContactName { get; set; }
+
         [StringLength(255)]
         public string ContactEmail { get; set; }
+
         [Required]
         public string ContactPhone { get; set; }
 
         public DateTime LastUpdate { get; set; }
+
+        //adding a new collection for using VehicleFeatures class to be able to make a bridge between vehicles and features
+        public ICollection<VehicleFeature> Features { get; set; }
+
+        //initializing the Vehicle const
+        public Vehicle()
+        {
+            Features = new Collection<VehicleFeature>();
+        }
+
 
 
     }
