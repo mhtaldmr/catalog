2d6e746114fa38860f3af0db5e48253d17b05bf6
mhtaldmr <mht.alidemir@gmail.com>
2022-03-22 11:17:28 +0300

    DeleteAPI added.

--- Vega/Controllers/VehiclesController.cs
+++ Vega/Controllers/VehiclesController.cs
@@ -64,13 +64,20 @@ namespace Vega.Controllers
             return Ok(result);
         }
 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+                return NotFound();
+
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
 
 
-
-
-
-
-
+            return Ok(id);
+        }
 
     }
 }
